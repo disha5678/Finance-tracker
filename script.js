05feabfbd3e983730c7f9d0b1588e007e7101e35
disha <dishasingla2005@gmail.com>
2025-01-31 18:12:55 +0530

    added function to js file

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 function showRegistrationForm() {
     document.getElementById('registrationForm').style.display = 'block';
+    document.getElementById('loginForm').style.display = 'none'; 
 }
 
+function showLoginForm(){
+    document.getElementById('loginForm').style.display = 'block';
+    document.getElementById('registrationForm').style.display = 'none'; 
+}
