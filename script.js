f1b310cee03051b4748003a8370a6141aa4bc303
disha5678 <147481806+disha5678@users.noreply.github.com>
2025-03-12 02:20:28 +0530

    shifted to public folder

--- script.js
+++
@@ -1,9 +0,0 @@
-function showRegistrationForm() {
-    document.getElementById('registrationForm').style.display = 'block';
-    document.getElementById('loginForm').style.display = 'none'; 
-}
-
-function showLoginForm(){
-    document.getElementById('loginForm').style.display = 'block';
-    document.getElementById('registrationForm').style.display = 'none'; 
-}
